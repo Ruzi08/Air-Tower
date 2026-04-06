76e53c476664f17aefe95e78e9c34572adfdaa9d
Ruzi <ruzix06@gmail.com>
2026-04-07 00:01:35 +0300

    feat: add interacting with objects mechanic

---
+++ My project/Assets/_Project/Scripts/Mechanics/Interaction/LightSwitch.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+// Обрати внимание: мы добавляем , IInteractable после MonoBehaviour
+public class LightSwitch : MonoBehaviour, Interactable
+{
+    public Light targetLight;
+    private bool isLightOn = false;
+
+    // Обязательный метод из нашего интерфейса
+    public void Interact()
+    {
+        isLightOn = !isLightOn;
+        
+        if (targetLight != null)
+        {
+            targetLight.enabled = isLightOn;
+        }
+        
+        Debug.Log("Игрок щелкнул выключателем!");
+    }
+
+    public string GetDescription()
+    {
+        return "Переключить свет";
+    }
+}
